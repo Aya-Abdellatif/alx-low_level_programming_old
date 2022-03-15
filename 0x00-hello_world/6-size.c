960f8722c2909435d24301c1c86d9915458b8597
Hannah Ochim <hannahochim@gmail.com>
2022-03-15 15:21:48 -0700

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Print sizes of various types 
+ *
+ * Description: This program prints the size of various types on the computer
+ *
+ * return - 0
+ */
+
+int main(void)
+{
+
+	return (0);
+}
