1fd6e2c001c477643b3e39decc4e2a7988861153
Hannah Ochim <hannahochim@gmail.com>
2022-03-15 15:37:51 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,9 +4,8 @@
  *
  * Description: This program prints the size of various types on the computer
  *
- * return - 0
+ * return: 0 (Success)
  */
-
 int main(void)
 {
 	char c;
@@ -14,12 +13,10 @@ int main(void)
 	long li;
 	long long lli;
 	float f;
-
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
-
 	return (0);
 }
