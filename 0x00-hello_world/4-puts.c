04a7fb86a2f6319eb3442fd8c5da7ee64f542fdd
Hannah Ochim <hannahochim@gmail.com>
2022-03-15 14:24:39 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Description - This code prints a string with puts function
+ *
+ * Return - 0
+ */
+
+int main()
+{
+	puts("Programming is like building a multilingual puzzle.\n");
+	getchar();
+	return (0);
+}
