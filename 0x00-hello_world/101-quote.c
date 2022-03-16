fb8d30cb4777ed540540cc530c58aabf416e8542
Hannah Ochim <hannahochim@gmail.com>
2022-03-16 00:43:51 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Main function contains a string that prints to stderr
+ *
+ * Description: Program that prints to the standard error
+ *
+ * return: Returns 1
+ */
+
+int main(void)
+{
+	fwrite("and that piece of art is useful""- Dora Korpar, 2015-10-19\n", stderr);
+	return(1);
+}
