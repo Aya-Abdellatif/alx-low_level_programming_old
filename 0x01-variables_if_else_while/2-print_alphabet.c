47d679325ff31bcef6aca4c5a25d16555391a468
Hannah Ochim <hannahochim@gmail.com>
2022-03-17 10:23:07 -0700

    Lower case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 /**
  * main - This program prints lowercase alphabets
- * Description: putchar function employed to print lowercase alphabet followed by a new line
+ * Description: putchar function employed to
+ * print lowercase alphabet followed by a new line
  * Return: returns 0 on success
  */
-int main (void)
+
+int main(void)
 {
-	putchar 
+	char ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+{
+	putchar(ch);
+	putchar('\n');
+}
+	return(0);
+}
