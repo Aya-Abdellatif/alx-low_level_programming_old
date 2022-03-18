a3b63fcb67ffca3ede6655f725df6c0579343797
Hannah Ochim <hannahochim@gmail.com>
2022-03-18 09:40:59 -0700

    Print combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,12 +14,15 @@ int main(void)
 {
 	int k;
 
-	for (k = '0'; k < '9'; k++)
+	for (k = '0'; k <= '9'; k++)
 	{
 		putchar(k);
+		if (k != '9')
+		{
 		putchar(',');
 		putchar(' ');
+		}
 	}
-	putchar('9''\n');
+	putchar('\n');
 	return (0);
 }
