3d42e83db1322160c78fb3fc58bd77f9da9e7dc9
Hannah Ochim <hannahochim@gmail.com>
2022-03-18 09:03:56 -0700

    Print combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,21 +3,23 @@
  * main - Program prints all possible
  * combinations of single-digit numbers.
  *
- * Description: 
- * Return - return 0 on success
+ * Description: This program is designed to print
+ * all possible combinations of single-digits in
+ * ascending order using the putchar() function.
+ *
+ * Return: returns 0 on success.
  */
 
-int main(void)	
+int main(void)
 {
 	int k;
 
-	for(k = '0'; k <= '9'; k++)
+	for (k = '0'; k < '9'; k++)
 	{
-		putchar(k);
 		putchar(k);
 		putchar(',');
 		putchar(' ');
-
 	}
+	putchar('9');
 	return (0);
 }
