144b810b17eee58bced8e741e67875dcf5317517
Hannah Ochim <hannahochim@gmail.com>
2022-03-17 21:49:08 -0700

    Print alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**main - Program prints lower case alphabet
+ * Description: This program prints lower case
+ * alphabets in reverse, followed by a new line
+ * Return: returns 0 on success
+ */
+
+int main(void)
+{
+	char ch='z';
+
+		for (ch='z'; ch>='a'; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
