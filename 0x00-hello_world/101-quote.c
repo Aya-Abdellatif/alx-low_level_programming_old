a00a793264cf029be02dcaaa1097b90e070634b5
Hannah Ochim <hannahochim@gmail.com>
2022-03-19 00:42:32 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 /**
  * main - Main function contains a string that prints to stderr
  * Description: Program that prints to the standard error
- * return: Returns 1 on success
+ * Return: returns 1 on success
  */
 
 int main(void)
