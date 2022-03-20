decde71fe8e5ce30186a452db754a14e15393047
Hannah Ochim <hannahochim@gmail.com>
2022-03-20 04:47:02 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Program prints a string
+ * Description: This program prints _putchar followed by
+ * a new line.
+ * Return: returns 0 on success
+ */
+
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+}
