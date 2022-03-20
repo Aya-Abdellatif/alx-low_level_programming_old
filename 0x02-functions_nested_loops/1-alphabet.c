a70d35d12324ff551995282d1f8689af8e4f6767
Hannah Ochim <hannahochim@gmail.com>
2022-03-20 16:31:44 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * print_alphabet - Entry point
+ * Description: A function that prints the alphabet, in lowercase
+ * Return: void
+ */
+
 void print_alphabet(void)
 {
 	char ch = 'a';
