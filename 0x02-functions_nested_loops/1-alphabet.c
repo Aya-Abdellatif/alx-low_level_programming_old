948c7179104e79738182a88ac718fc63f872c4f3
Hannah Ochim <hannahochim@gmail.com>
2022-03-20 16:24:18 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,11 @@
 
 void print_alphabet(void)
 {
-        char ch = 'a';
-
+	char ch = 'a';
+	
 	for (ch = 'a'; ch <= 'z'; ch++)
-
-        {
-                _putchar(ch);
-        }
+	{
+		_putchar(ch);
+	}
 	_putchar('\n');
 }
