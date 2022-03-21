abbc415498af3598524cfa1721042df142089edf
Hannah Ochim <hannahochim@gmail.com>
2022-03-20 18:15:41 -0700

    _isalpha

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,22 +2,23 @@
 
 /**
  * _islower - Entry point
- * Description: A function that checks for lowercase character
+ * Description: A function that
+ * checks for lowercase character
  * @c: the integer value it receives
  * Return: 1 if TRUE 0 if FALSE
  */
+
 int _islower(int c)
 {
-int i = 'a';
-
-for (i = 'a'; i <= 'z'; i++)
-{
-/* refer int c*/
-if (c == i)
-{
-return (1);
+	int i = 'a';
+	
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		/* refer int c*/
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
-}
-return (0);
-}
-
